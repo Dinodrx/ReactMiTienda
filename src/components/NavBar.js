1ce6858397a39d69e522503de3bc65ed0f1cec9f
Jose Escurra Ayala <95514022+Dinodrx@users.noreply.github.com>
2022-06-02 22:59:36 -0500

    Add files via upload

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return <header>
+        <h1>Mi Tienda</h1>
+        <ul>
+            <a href="#">Inicio</a>
+            <a href="#">Contacto</a>
+            <a href="#">Nosotros</a>
+        </ul>
+    </header>
+}
+
+
+export default NavBar
